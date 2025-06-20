9c0e5d768e9b7f3ded28284c261c562b4569d4b0
minki-jeon <minki.jeon.study@gmail.com>
2025-06-20 11:41:09 +0900

    게시물 목록 - Repository (Paging)

--- src/main/java/com/example/prj1/repository/BoardRepository.java
+++ src/main/java/com/example/prj1/repository/BoardRepository.java
@@ -2,11 +2,16 @@ package com.example.prj1.repository;
 
 import com.example.prj1.dto.BoardListInfo;
 import com.example.prj1.entity.Board;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Integer> {
 
+    // 전체 목록 조회
     List<BoardListInfo> findAllBy();
+
+    // 전체 목록 조회 페이징
+    List<BoardListInfo> findAllBy(Pageable pageable);
 }
