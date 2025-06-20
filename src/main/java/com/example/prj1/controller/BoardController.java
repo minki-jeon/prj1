3841740ceef66df3fcaf6899423084914e102d50
minki-jeon <minki.jeon.study@gmail.com>
2025-06-20 10:46:33 +0900

    게시물 목록 - Controller(get)

--- src/main/java/com/example/prj1/controller/BoardController.java
+++ src/main/java/com/example/prj1/controller/BoardController.java
@@ -4,9 +4,12 @@ import com.example.prj1.dto.BoardForm;
 import com.example.prj1.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,4 +30,16 @@ public class BoardController {
 
         return "board/write";
     }
+
+    @GetMapping("list")
+    public String list(Model model) {
+
+
+        var result = boardService.list();
+
+        model.addAttribute("boardList", result);
+
+        return "board/list";
+    }
+
 }
