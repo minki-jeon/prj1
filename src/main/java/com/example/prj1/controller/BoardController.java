06d9c0905d407317e9a075efa14a24b99ba0ced8
minki-jeon <minki.jeon.study@gmail.com>
2025-06-20 10:33:16 +0900

    게시물 작성 - controller(Get/Post)

--- src/main/java/com/example/prj1/controller/BoardController.java
+++ src/main/java/com/example/prj1/controller/BoardController.java
@@ -1,8 +1,11 @@
 package com.example.prj1.controller;
 
+import com.example.prj1.dto.BoardForm;
+import com.example.prj1.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,8 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("board")
 public class BoardController {
 
+    private final BoardService boardService;
+
     @GetMapping("write")
-    public String write() {
+    public String writeView() {
+
+        return "board/write";
+    }
+
+    @PostMapping("write")
+    public String write(BoardForm data) {
+        boardService.add(data);
 
         return "board/write";
     }
