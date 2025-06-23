fbb71d3e861bc25f23b274bf9bce2b3b73406338
minki-jeon <minki.jeon.study@gmail.com>
2025-06-23 11:32:45 +0900

    회원가입 FORM/DTO

--- src/main/java/com/example/prj1/member/controller/MemberController.java
+++ src/main/java/com/example/prj1/member/controller/MemberController.java
@@ -1,9 +1,14 @@
 package com.example.prj1.member.controller;
 
+import com.example.prj1.dto.MemberForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,4 +19,16 @@ public class MemberController {
     public String signupFrom() {
         return "member/signup";
     }
+
+    @PostMapping("signup")
+    public String signup(MemberForm data, RedirectAttributes rttr) {
+        //service
+        System.out.println(data);
+
+        rttr.addFlashAttribute("alert", Map.of("code", "", "message", "회원가입이 완료되었습니다."));
+
+        return "redirect:/board/list";
+
+    }
+
 }
