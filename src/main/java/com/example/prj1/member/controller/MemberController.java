edc117f9079f0f0d7c3c1ae3462435234a5bc058
minki-jeon <minki.jeon.study@gmail.com>
2025-06-24 14:34:18 +0900

    회원 정보 본인만 확인 가능하도록 처리

--- src/main/java/com/example/prj1/member/controller/MemberController.java
+++ src/main/java/com/example/prj1/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.example.prj1.member.controller;
 
+import com.example.prj1.member.dto.MemberDto;
 import com.example.prj1.member.dto.MemberForm;
 import com.example.prj1.member.service.MemberService;
 import jakarta.servlet.http.HttpSession;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Map;
@@ -55,10 +57,22 @@ public class MemberController {
     }
 
     @GetMapping("view")
-    public String view(String id, Model model) {
-        model.addAttribute("member", memberService.get(id));
+    public String view(String id,
+                       @SessionAttribute(value = "loggedInUser", required = false) MemberDto user,
+                       RedirectAttributes rttr,
+                       Model model) {
+        MemberDto member = memberService.get(id);
 
-        return "member/view";
+        if (user != null) {
+            if (member.getId().equals(user.getId())) {
+                model.addAttribute("member", member);
+
+                return "member/view";
+            }
+        }
+        rttr.addFlashAttribute("alert", Map.of("code", "warning", "message", " 권한이 없습니다."));
+
+        return "redirect:/board/list";
     }
 
     @PostMapping("remove")
